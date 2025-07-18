df654bad4ab39334ca37401079be6efbfcc54fe4
Turbold <turbolddev@gmail.com>
2025-07-18 13:22:49 +0800

    mongo

--- lib/config/mongodb.ts
+++ lib/config/mongodb.ts
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://turbold:Hello0220@goblin.uirlr.mongodb.net/?retryWrites=true&w=majority&appName=Goblin";
 if (!MONGODB_URI) {
   throw new Error("Please define the MONGODB_URI environment variable");
 }
